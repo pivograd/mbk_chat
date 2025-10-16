90bcbbfc6205b906d7c7973c2025f12d76011bd4
pivograd <e.pivograd@yandex.ru>
2025-10-16 15:43:13 +0300

    добавил try except в init_db

--- db/core.py
+++ db/core.py
@@ -11,7 +11,10 @@ from db.models.bx_handler_process import BxHandlerProcess  # noqa: F401
 from db.models.bx_processed_call import BxProcessedCall  # noqa: F401
 from db.models.contact_routing import ContactRouting  # noqa: F401
 from db.models.rr_cursor import RRCursor  # noqa: F401
-from db.models.transport_activation import TransportActivation, bootstrap_transport_activation  # noqa: F401
+from db.models.transport_activation import TransportActivation # noqa: F401
+from db.models.transport_activation import bootstrap_transport_activation
+from telegram.send_log import send_dev_telegram_log
+
 
 def make_engine() -> AsyncEngine:
     return create_async_engine(
@@ -21,16 +24,20 @@ def make_engine() -> AsyncEngine:
     )
 
 async def init_db(app):
-    engine = make_engine()
-    app["db_engine"] = engine
+    try:
+        engine = make_engine()
+        app["db_engine"] = engine
 
-    local_session = async_sessionmaker(bind=engine, expire_on_commit=False)
-    app["db_sessionmaker"] = local_session
+        local_session = async_sessionmaker(bind=engine, expire_on_commit=False)
+        app["db_sessionmaker"] = local_session
 
-    Bx24Deal.configure_sessionmaker(local_session)
+        Bx24Deal.configure_sessionmaker(local_session)
 
-    async with local_session() as session:
-        await bootstrap_transport_activation(session)
+        async with local_session() as session:
+            await bootstrap_transport_activation(session)
+        await send_dev_telegram_log('[init_db]\nОтрабтал без ошибок', 'DEV')
+    except Exception as e:
+        await send_dev_telegram_log(f'[init_db]\nОШИБКА\n {e}', 'ERROR')
 
 async def close_db(app):
     engine: AsyncEngine = app["db_engine"]
