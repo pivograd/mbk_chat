f0c01f999e4a70b584f615d9e009d6c5cc423639
pivograd <e.pivograd@yandex.ru>
2025-10-27 18:08:28 +0300

    убрал лог из trust_me

--- trust_me.py
+++ trust_me.py
@@ -4,5 +4,5 @@ from telegram.send_log import send_dev_telegram_log
 
 
 async def trust_me(request: web.Request):
-    await send_dev_telegram_log('[TRUSTME]', 'DEV')
+    # await send_dev_telegram_log('[TRUSTME]', 'DEV')
     return web.Response(status=200, text=f"Йоу йоу йоу йоу, мой друг под кислотой, йоу йоу")
