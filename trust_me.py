529a1b10441f2b88d9845387419017416bae09ce
pivograd <e.pivograd@yandex.ru>
2025-10-27 18:06:04 +0300

    добавил request в trust_me

--- trust_me.py
+++ trust_me.py
@@ -3,6 +3,6 @@ from aiohttp import web
 from telegram.send_log import send_dev_telegram_log
 
 
-async def trust_me():
+async def trust_me(request: web.Request):
     await send_dev_telegram_log('[TRUSTME]', 'DEV')
     return web.Response(status=200, text=f"Йоу йоу йоу йоу, мой друг под кислотой, йоу йоу")
