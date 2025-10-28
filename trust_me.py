a0ab73b6245d3962fad69d2f264be532fea60d23
pivograd <e.pivograd@yandex.ru>
2025-10-28 10:53:44 +0300

    сделал прокси на локальный обработчик trust_me

--- trust_me.py
+++ trust_me.py
@@ -1,8 +1,27 @@
-from aiohttp import web
+from aiohttp import web, ClientSession
 
 from telegram.send_log import send_dev_telegram_log
 
 
 async def trust_me(request: web.Request):
-    # await send_dev_telegram_log('[TRUSTME]', 'DEV')
+    await send_dev_telegram_log('[TRUSTME]', 'DEV')
+    DEV_TUNNEL = 'https://37f028b5-aae7-485e-bb8b-a6cfff3186fe.tunnel4.com'
+    body = await request.read()
+    target = f"{DEV_TUNNEL}{request.path_qs}"
+
+    headers = dict(request.headers)
+    headers.pop("Host", None)
+    headers.pop("Content-Length", None)
+
+    async with ClientSession() as s:
+        async with s.request(
+                request.method, target, data=body, headers=headers,
+                ssl=False, allow_redirects=False
+        ) as r:
+            ...
+            # return web.Response(
+            #     status=r.status,
+            #     body=await r.read(),
+            #     headers=r.headers
+            # )
     return web.Response(status=200, text=f"Йоу йоу йоу йоу, мой друг под кислотой, йоу йоу")
