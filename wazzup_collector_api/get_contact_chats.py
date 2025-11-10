a1538d07466e0a3cd10ac8494571fde79dad743d
pivograd <e.pivograd@yandex.ru>
2025-11-10 17:24:30 +0300

    исправил ошибочную обработку 404 (NotFound)

--- wazzup_collector_api/get_contact_chats.py
+++ wazzup_collector_api/get_contact_chats.py
@@ -8,10 +8,14 @@ async def get_contact_chats(contact_phone: str) -> list[dict]:
         url = f'https://wazzup.mbk-chat.ru/api/contact/chats/history?contact_phone={contact_phone}'
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
+                if resp.status == 404:
+                    data = await resp.json()
+                    return data
                 resp.raise_for_status()
                 data = await resp.json()
-                if not data.get('ok'):
-                    return data
+
+        if not data.get('ok'):
+            return data
 
         return data.get('conversations')
 
