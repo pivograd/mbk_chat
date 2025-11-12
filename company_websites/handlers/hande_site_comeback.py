e7c67d0f76eacc09db06be548cdf6d6b3e07c83a
pivograd <e.pivograd@yandex.ru>
2025-11-12 16:19:33 +0300

    добавил вебхук для отслеживания возвращения на сайт пользователей

---
+++ company_websites/handlers/hande_site_comeback.py
@@ -0,0 +1,23 @@
+from aiohttp import web
+
+from telegram.send_log import send_dev_telegram_log
+from utils.normalize_phone import normalize_phone
+
+
+async def handle_site_comeback(request):
+    """
+    Обработчик заявок с сайта LEADON
+    """
+    data = await request.json()
+    try:
+        phone = normalize_phone(data.get("phone", ''))
+        domain = data.get("domain")
+        if not domain or not phone:
+            return web.Response(text="❌ Не указан телефон или домен", status=400)
+
+        await send_dev_telegram_log(f'[handle_site_comeback]\ndata: {data}')
+        return web.Response(text="Повторное посещение зафиксированно!", status=200)
+    except Exception as e:
+        await send_dev_telegram_log(f'[handle_site_comeback]\nКритическая ошибка\nDATA: {data}\nERROR: {e}',
+                                    'ERROR')
+        return web.Response(text='Серверная ошибка', status=500)
