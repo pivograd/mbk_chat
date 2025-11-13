cab0d4f8ac45af3fc7ec14967f757f10ae465f35
pivograd <e.pivograd@yandex.ru>
2025-11-13 12:16:36 +0300

    добавил прокидывание информации про повторное посещение сайта в приватные сообщения связанных диалогов CW

--- company_websites/handlers/hande_site_comeback.py
+++ company_websites/handlers/hande_site_comeback.py
@@ -1,8 +1,9 @@
 from aiohttp import web
 
+from chatwoot_api.chatwoot_client import ChatwootClient
 from telegram.send_log import send_dev_telegram_log
 from utils.normalize_phone import normalize_phone
-
+from chatwoot_api.functions.safe_send_to_chatwoot import safe_send_to_chatwoot
 
 async def handle_site_comeback(request):
     """
@@ -10,11 +11,15 @@ async def handle_site_comeback(request):
     """
     data = await request.json()
     try:
-        phone = normalize_phone(data.get("phone", ''))
+        phone = normalize_phone(data.get("phone", '')).lstrip('+')
         domain = data.get("domain")
         if not domain or not phone:
             return web.Response(text="❌ Не указан телефон или домен", status=400)
-
+        async with ChatwootClient() as cw:
+            contact_id = await cw.get_contact_id(phone)
+            conversations = await cw.get_conversations(contact_id)
+            for conversation in conversations:
+                await cw.send_message(conversation['id'], f'Клиент посетил наш сайт: {domain}', private=True)
         await send_dev_telegram_log(f'[handle_site_comeback]\ndata: {data}', 'DEV')
         return web.Response(text="Повторное посещение зафиксированно!", status=200)
     except Exception as e:
