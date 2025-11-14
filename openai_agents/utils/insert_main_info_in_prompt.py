d76f4fa86be65b81a7e20934d33aef6c35a1b3dd
pivograd <e.pivograd@yandex.ru>
2025-11-14 18:20:46 +0300

    fix

--- openai_agents/utils/insert_main_info_in_prompt.py
+++ openai_agents/utils/insert_main_info_in_prompt.py
@@ -9,7 +9,9 @@ def insert_main_info_in_prompt(prompt: str, cfg: OpenAIConfig) -> str:
     Подставляет в промт основную информацию на место <<MAIN_INFO>>
     """
     main_block = read_txt_file(MAIN_BLOCK_PATH)
+    catalogs = read_txt_file(cfg.catalogs_path)
 
+    main_block = main_block.replace( '<<CATALOGS_BLOCK>>', catalogs)
     main_block = main_block.replace('<<PRICE_COMPLECTATION>>', cfg.price_complectation)
     main_block = main_block.replace('<<FOUNDATION_SIZE>>', cfg.foundation_size)
     main_block = main_block.replace('<<GLUED_BEAM_SIZE>>', cfg.glued_beam_size)
@@ -19,6 +21,5 @@ def insert_main_info_in_prompt(prompt: str, cfg: OpenAIConfig) -> str:
     main_block = main_block.replace('<<GEOGRAPHY>>', cfg.geography)
     main_block = main_block.replace('<<OFFICE_ADDRESS>>', cfg.office_address)
     main_block = main_block.replace('<<WEBSITE>>', cfg.website)
-    main_block = insert_txt_in_block(main_block, cfg.catalogs_file, '<<CATALOGS_BLOCK>>')
 
     return prompt.replace('<<MAIN_INFO>>', main_block)
